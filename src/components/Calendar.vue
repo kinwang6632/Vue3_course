63fad9de9a9453d641f1439bb81f6ccaf64fbf06
unknown <kinwang6632@hotmail.com>
2021-07-01 20:57:38 +0800

    Calendar Days OK 02:59:17

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,21 +1,65 @@
 <template>
   <div class="m-auto">
     <h1 class="text-2xl my-2 text-center">Vue Calendar</h1>
-    <section class="flex">
-      <p class="w-10 h-10 text-center" v-for="day in days" :key="day">{{ day }}</p>
+    <section class="mx-5 flex justify-between">
+      <h2 class="font-bold">{{ currentMonthName }}</h2>
+      <h2 class="font-bold">{{ currentYear }}</h2>
     </section>
-    <section class="flex">
-      <p class="w-10 h-10 text-center" v-for="num in 3" :key="num">{{ num }}</p>
+
+    <section class="flex my-2">
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="day in days"
+        :key="day"
+      >
+        {{ day }}
+      </p>
     </section>
+    <section class="flex flex-wrap">
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="num in startDay()"
+        :key="num"
+      >
+        <!-- {{ num }} -->
+      </p>
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="num in daysInMonth(currentYear, currentYear)"
+        :key="num"
+      >
+        {{ num }}
+      </p>
+    </section>
+    <!-- v-for="num in daysInMonth(currentYear, currentMonth)" -->
   </div>
+  <!-- v-for="num in daysInMonth(currentYear, currentMonth)" -->
 </template>
+    
 
 <script>
 export default {
   data() {
     return {
+      //   currentMonth: new Date().getMonth() + 1 ,
+      //   currentMonthName: new Date().toLocaleString("Default", { month: "long" }),
+      //   currentYear: new Date().getFullYear(),
+      currentMonth: new Date().getMonth() + 1,
+      currentMonthName: new Date().toLocaleString("Default", { month: "long" }),
+      currentYear: new Date().getFullYear(),
       days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     };
+  },
+  methods: {
+    daysInMonth(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
+    startDay() {
+      return new Date(this.currentYear, this.currentMonth - 1, 1).getDay()
+    },
   },
 };
 </script>
