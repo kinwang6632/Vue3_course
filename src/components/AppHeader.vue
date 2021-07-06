14c44918747061f6c4420af4b44c5de753854745
unknown <kinwang6632@hotmail.com>
2021-07-06 20:26:42 +0800

    markedown UI complete

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full">
+  <div class="w-full text-justify">
     <nav
       class="
         w-full
--- src/pages/Markdown.vue
+++ src/pages/Markdown.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-      markdown
+  <div class="flex w-full flex-wrap">
+    <h1 class="w-full text-3xl my-4">Markdown App</h1>
+    <section class="flex m-auto w-10/12 text-justify h-screen">
+      <article class="w-1/2 border">
+        <textarea class="w-full h-full" v-model="text"></textarea>
+      </article>
+      <article class="w-1/2 border bg-gray-100">{{marked(text)}}</article>
+    </section>
   </div>
 </template>
 
 <script>
+import marked from 'marked'
 export default {
-
-}
+  data() {
+    return { text: "", };
+  },
+};
 </script>
 
 <style>
-
 </style>
