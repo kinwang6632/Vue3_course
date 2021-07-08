1ee2f7db2a3911a25fc81a094a93a3bb9037fb5a
unknown <kinwang6632@hotmail.com>
2021-07-08 14:51:41 +0800

    slider full complete 04:37:34

--- src/pages/Slider.vue
+++ src/pages/Slider.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="flex w-full flex-wrap">
-      <div class="absoulte w-full" v-for="(color,index) in sliders" :key="color">
-          <div
-            :class="color"
-            style="height:350px"
-            v-if="currentSlide == index"
-          >              
-          </div>
+    <div class="absolute w-full" v-for="(color, index) in sliders" :key="color">
+      <transition name="fade">
+        <div
+          :class="color"
+          style="height: 350px"
+          v-if="currentSlide == index"
+        ></div>
+      </transition>
+    </div>
+    <div class="absolute w-full flex justify-center" style="height: 340px">
+      <div class="absolute bottom-0 flex justify-center">
+        <div
+          v-for="(slider,index) in sliders"
+          :key="slider"
+          @click="makeActive(index)"
+          :class="currentSlide == index ? 'bg-gray-700':'bg-gray-300'"
+          class="w-4 mx-2 h-4 rounded-full cursor-pointer shadow-m"
+        ></div>
       </div>
+    </div>
+    <div class="my-10 flex w-full">
+      <div class="m-auto">
+        <transition name="fade">
+          <h1 v-if="isTitleShowing">Slider Carosel</h1>
+        </transition>
+        <button
+          @click="isTitleShowing = !isTitleShowing"
+          class="px-2 rounded border"
+        >
+          Toggle Text
+        </button>
+      </div>
+    </div>
     <!-- <div
       v-if="currentSlide === 0"
       class="absolute w-full bg-blue-600"
@@ -32,12 +57,18 @@ export default {
     return {
       currentSlide: 1,
       interval: "",
-      sliders:['bg-blue-600','bg-yellow-600','bg-red-600']
+      sliders: ["bg-blue-600", "bg-yellow-600", "bg-red-600"],
+      isTitleShowing: true,
     };
+  },
+  methods:{
+    makeActive(index) {
+      this.currentSlide = index
+    }
   },
   mounted() {
     this.interval = setInterval(() => {
-        // console.log(this.currentSlide)
+      // console.log(this.currentSlide)
       if (this.currentSlide === 2) {
         this.currentSlide = 0;
       } else {
@@ -52,4 +83,18 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  /* transition: opacity 0.5s ease; */
+  transition: all 1s ease;
+}
+
+.fade-enter-from {
+  opacity: 0;
+  transform: translate(-100%);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translate(100%);
+}
 </style>
