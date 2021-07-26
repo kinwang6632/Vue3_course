625016ba7f2324b87647a8e9331fe90f50250926
unknown <kinwang6632@hotmail.com>
2021-07-26 18:39:06 +0800

    avator screen ok 09:00:29

--- src/pages/UserCrud.vue
+++ src/pages/UserCrud.vue
@@ -1,16 +1,60 @@
 <template>
   <section class="flex w-full">
     <div class="m-auto">
-      <div>
-        <ul>
-          <li v-for="user in state.users.data" :key="user.id">
-            <p>
-              {{ user.first_name }}
-            </p>
-            <p>{{ user.last_name }}</p>
-            <p>{{ user.email }}</p>
-          </li>
-        </ul>
+      <div class="mt-10">
+        <table>
+          <thead>
+            <tr>
+              <th class="px-4 py-2 border">ID</th>
+              <th class="px-4 py-2 border">Avatar</th>
+              <th class="px-4 py-2 border">First Name</th>
+              <th class="px-4 py-2 border">Last Name</th>
+              <th class="px-4 py-2 border">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in state.users.data" :key="user.id">
+              <td class="border px-4 py-2">
+                {{ user.id }}
+              </td>
+              <td class="border px-4 py-2">
+                <img
+                  :src="user.avatar"
+                  :alt="user.first_name"
+                  width="50"
+                  class="rounded-full"
+                />
+              </td>
+              <td class="border px-4 py-2">
+                {{ user.first_name }}
+              </td>
+              <td class="border px-4 py-2">
+                {{ user.last_name }}
+              </td>
+              <td class="border px-4 py-2">
+                {{ user.email }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="flex justify-between">
+          <button
+            class="px-3 py-2 border rounded"
+            @click="prev"
+            :disabled="state.users.page === 1"
+            :class="state.users.page === 1 ? 'bg-gray-100' : 'hover:shadow'"
+          >
+            Prev
+          </button>
+          <button
+            class="px-3 py-2 border rounded"
+            @click="next"
+            :disabled="state.users.page === state.users.total_pages"
+            :class="state.users.page === state.users.total_pages ? 'bg-gray-100' : 'hover:shadow'"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   </section>
@@ -27,8 +71,17 @@ export default {
     onMounted(async () => {
       const { data } = await axios.get("https://reqres.in/api/users");
       state.users = data;
+      console.log(state.users);
     });
-    return { state };
+    async function next() {
+      const { data } = await axios.get("https://reqres.in/api/users?page=2");
+      state.users = data;
+    }
+    async function prev() {
+      const { data } = await axios.get("https://reqres.in/api/users?page=1");
+      state.users = data;
+    }
+    return { state, next, prev };
   },
 };
 </script>
